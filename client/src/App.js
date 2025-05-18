59d5bd0e19b4a9f3c28484a0690068c5e99eabbd
AdiSchiff <adidi.schiff@gmail.com>
2025-05-18 19:51:41 +0300

    adding await to getSocket

--- client/src/App.js
+++ client/src/App.js
@@ -14,13 +14,13 @@ import AdminRoute from './components/AdminRoute';
 import SongSelectedRoute from './components/SongSelectedRoute';
 
 function App() {
-    useEffect(() => {
+    useEffect(async () => {
         const token = localStorage.getItem('token');
         const username = localStorage.getItem('username');
         const role = localStorage.getItem('role');
 
         if (token) {
-            const socket = initSocket(token);
+            const socket = await initSocket(token);
             socket.emit('join-session', {
                 username,
                 role
